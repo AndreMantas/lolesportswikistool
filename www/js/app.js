8ea1d5f47c091107e4f79425f013ce858bcd21f4
Andre Mantas <andre.mantas@inw-tech.com>
2017-03-23 01:39:40 +0000

    Creates simple angular app, controller and directive

---
+++ www/js/app.js
@@ -0,0 +1,3 @@
+// Style recommended from: https://github.com/johnpapa/angular-styleguide/blob/master/a1/README.md
+angular
+    .module('MatchHistoryApp', []);
---
+++ www/js/controllers/createMH.controller.js
@@ -0,0 +1,30 @@
+﻿angular
+    .module('MatchHistoryApp')
+    .controller('CreateMHCtrl', CreateMHCtrl)
+    .directive('createMatchHistory', createMatchHistory);
+
+function CreateMHCtrl($scope, $http) {
+    $scope.playersPerTeam = 5;
+
+    $scope.toggleShow = function () {
+		
+    };
+
+    //$scope.getCardsList = function () {
+        //$http.get("...")
+        //    .then(function (result) {
+        //        
+        //    }, function (result) {
+        //        //some error
+        //        console.log(result);
+        //    });
+    //};
+};
+
+function createMatchHistory() {
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/create-match-history.html',
+        controller: 'CreateMHCtrl'
+    }
+};
