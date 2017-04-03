ec18755010810bbaf9424dbf7ab8a2a81aa3843a
Andre Mantas <andre.mantas@inw-tech.com>
2017-04-04 00:22:08 +0100

    Improves model binding and removes some duplicate code

--- www/js/controllers/createMH.controller.js
+++ www/js/controllers/createMH.controller.js
@@ -6,15 +6,29 @@
 function CreateMHCtrl($scope, $http) {
 	
     $scope.playersPerTeam = [1,2,3,4,5];
-	$scope.bansPerTeam    = [1,2,3,4,5];
-	
-	$scope.teamA = "tsm";
-	$scope.teamB = "clg";
-	
-	$scope.blueBans = [];
-	$scope.redBans = [];
-	$scope.bluePicks = [];
-	$scope.redPicks = [];
+    $scope.bansPerTeam = [1, 2, 3, 4, 5];
+
+    $scope.teams = [
+        {
+            // blue team
+            name: '',
+            score: 0,
+            players: [],
+            bans: [],
+            picks: []
+        },
+        {
+            // red team
+            name: '',
+            score: 0,
+            players: [],
+            bans: [],
+            picks: []
+        }
+    ];
+
+    $scope.blueTeam = $scope.teams[0];
+    $scope.redTeam = $scope.teams[1];
 	
 	$scope.output = $scope.teamA + $scope.teamB;
 
