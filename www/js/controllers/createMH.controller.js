8ed9c340887165bb235d6fe0cb889429bc44c9f1
Andre Mantas <andre.mantas@inw-tech.com>
2017-04-04 10:15:08 +0100

    Initial generate output functionality.
    Added some comments and improved spacing

--- www/js/controllers/createMH.controller.js
+++ www/js/controllers/createMH.controller.js
@@ -30,10 +30,32 @@ function CreateMHCtrl($scope, $http) {
     $scope.blueTeam = $scope.teams[0];
     $scope.redTeam = $scope.teams[1];
 	
-	$scope.output = $scope.teamA + $scope.teamB;
+    $scope.output = '';
 
-    $scope.toggleShow = function () {
-		
+    $scope.generateOutput = function () {
+        /*return "PicksAndBansS7 | team1= |team2= |team1score= |team2score= |winner= "
+              +"|blueban1=      |redban1="
+              +"|blueban2=      |redban2="
+              +"|blueban3=      |redban3="
+              +"|bluepick1=     |bluepick1role="
+              +"                                            |redpick1=    |redpick1role="
+              +"                                            |redpick2=    |redpick2role="
+              +"|bluepick2=     |bluepick2role="
+              +"|bluepick3=     |bluepick3role="
+              +"                                            |redpick3=    |redpick3role="
+              +"|blueban4=     |redban4="
+              +"|blueban5=     |redban5="
+              +"                                            |redpick4=    |redpick4role="
+              +"|bluepick4=     |bluepick4role="
+              +"|bluepick5=     |bluepick5role="
+              +"                                            |redpick5=    |redpick5role="
+            ;
+        */
+        var output = 'PicksAndBansS7';
+        for (var i = 0, n = 1; i < $scope.teams.length; i++, n++) {
+            output = output.concat('| team'+n+'=' + $scope.teams[i].name);
+        }
+        $scope.output = output;
     };
 
     //$scope.getCardsList = function () {
