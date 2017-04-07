3a9c44d0c1e2f7d5bfae845fe3daebd836fc8ecb
Linku <kylelin78@gmail.com>
2017-04-06 20:29:24 -0400

    Added a toTitleCase(str) function and a championlist.csv -> array function

---
+++ www/js/main.js
@@ -0,0 +1,18 @@
+$.post("../php/getJSON.php",
+	{ feed: "https://docs.google.com/spreadsheet/pub?key=0AneWTc0o_1bpdEVVczlDckt2aXpmX0tRUU01eUZMX3c&single=true&gid=2030699110&output=csv" }
+	).done(function(d) {
+		
+		console.log("champs:" + d);
+		
+		for (i in d) {
+		  $('#championlist')
+         .append($("<option></option>")
+                    .attr("value", toTitleCase(d[i]["Code"]))
+                    .text(d[i]["Name"])); 
+		}
+});
+
+function toTitleCase(str)
+{
+    return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
